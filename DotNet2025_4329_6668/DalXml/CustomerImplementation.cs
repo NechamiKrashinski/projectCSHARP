14e7b52607fcc527618ab22a5deda0cc6c7c74bf
NechamiKrashinski <N.0556734722@gmail.com>
2025-03-25 12:39:55 +0200

    Commit

---
+++ DotNet2025_4329_6668/DalXml/CustomerImplementation.cs
@@ -0,0 +1,150 @@
+﻿
+
+using DalApi;
+using DO;
+using System.Reflection;
+using System.Xml.Serialization;
+namespace Dal;
+
+internal class CustomerImplementation : ICustomer
+{
+    static object lockObject = new object();
+
+    private List<Customer> LoadCustomersListFromXml()
+    {
+        try
+        {
+            List<Customer> customersList;
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Customer>));
+            lock (lockObject)
+            {
+                //קבלת רשימת הלקוחות מהXML
+                using (FileStream fs = new FileStream("../xml/customers.xml", FileMode.Open, FileAccess.Read))
+                {
+                    customersList = serializer.Deserialize(fs) as List<Customer>;
+                }
+            }
+
+            if (customersList == null)
+            {
+                customersList = new List<Customer>();
+            }
+            return customersList;
+        }
+        catch
+        {
+            throw new DalExceptionErrorInReadFromXml("customer");
+        }
+
+
+    }
+    private void StoreCustomersListToXml(List<Customer> customersList)
+    {
+        try
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Customer>));
+            lock (lockObject)
+            {
+                //שומרים את הרשימה חזרה בקובץ הXML
+                using (FileStream fs = new FileStream("../xml/customers.xml", FileMode.Open, FileAccess.Write))
+                {
+                    serializer.Serialize(fs, customersList);
+                }
+            }
+        }
+        catch
+        {
+            throw new DalExceptionErrorInWriteToXml("customer");
+        }
+
+
+
+    }
+    public int Create(Customer item)
+    {
+        if (item == null)
+        {
+            throw new DalExceptionNullReceived("customer");
+        }
+        List<Customer> customersList;
+        customersList = LoadCustomersListFromXml();
+        //בדיקה האם קיים אובייקט עם אותו מזהה
+        bool b = customersList.Any(c => c.ID == item.ID);
+        if (b)
+        {
+            throw new DalExceptionIdIsAlreadyExistInTheList("customer");
+        }
+        customersList.Add(item);
+        StoreCustomersListToXml(customersList);
+
+        return item.ID;
+
+    }
+
+    public void Delete(int id)
+    {
+        List<Customer> customersList;
+        customersList = LoadCustomersListFromXml();
+        Customer c = Read(id);
+        customersList.Remove(c);
+        StoreCustomersListToXml(customersList);
+    }
+
+    public Customer? Read(int id)
+    {
+        List<Customer> customersList;
+        customersList = LoadCustomersListFromXml();
+        try
+        {
+            Customer c = customersList.Single(c => c?.ID == id);
+            StoreCustomersListToXml(customersList);
+            return c;
+        }
+        catch
+        {
+            throw new DalExceptionIdDoesNotExistInTheList("customer");
+        }
+
+    }
+
+    public Customer? Read(Func<Customer, bool>? filter)
+    {        
+        if (filter == null) return null;
+        List<Customer> customersList;
+        customersList = LoadCustomersListFromXml();
+        try
+        {
+            return customersList.Single(c => filter(c));
+        }
+        catch
+        {
+            throw new DalExceptionNoValuesByCondition("customer");
+        }
+    }
+
+    public List<Customer?> ReadAll(Func<Customer, bool>? filter = null)
+    {
+        List<Customer> customersList;
+        customersList = LoadCustomersListFromXml();
+        if (filter == null)
+        {
+            return new List<Customer?>(customersList);
+
+        }
+        return customersList.Where(c => filter(c)).ToList();
+
+    }
+
+    public void Update(Customer item)
+    {
+        if (item == null)
+        {
+            throw new DalExceptionNullReceived("customer");
+        }
+        List<Customer> customersList;
+        customersList = LoadCustomersListFromXml();
+        Delete(item.ID);
+        customersList.Add(item);
+        StoreCustomersListToXml(customersList);
+    }
+}
---
+++ DotNet2025_4329_6668/DalXml/ProductImplementation.cs
@@ -0,0 +1,135 @@
+﻿
+
+using DalApi;
+using DO;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Dal;
+
+internal class ProductImplementation :IProduct
+{
+    static object lockObject = new object();
+    public int Create(Product item)
+    {
+        if (item == null)
+        {
+            throw new DalExceptionNullReceived("Product");
+        }
+        List<Product> productsList = LoadProductFromXml();
+        //הוספת המוצר החדש לרשימה
+        Product p = item with { ID = Config.NextValProduct };
+        productsList.Add(p);
+        //כתיבת רשימת המוצרים לxml
+        StoreProductToXml(productsList);
+        return p.ID;
+
+    }
+
+    public void Delete(int id)
+    {
+        List<Product> productsList = LoadProductFromXml();
+        Product? p = Read(id);
+        productsList.Remove(p);
+        StoreProductToXml(productsList);
+    }
+
+    public Product? Read(int id)
+    {
+        try
+        {
+            List<Product> productsList = LoadProductFromXml();
+            Product p = productsList.Single(p => p?.ID == id);
+            StoreProductToXml(productsList);
+            return p;
+        }
+        catch
+        {
+            throw new DalExceptionIdDoesNotExistInTheList("Product");
+        }
+
+    }
+
+    public Product? Read(Func<Product, bool>? filter)
+    {
+        List<Product> productsList = LoadProductFromXml();
+
+        StoreProductToXml(productsList);
+        try
+        {
+            return productsList.Single(p => filter(p));
+        }
+        catch
+        {
+            throw new DalExceptionNoValuesByCondition("customer");
+        }
+    }
+
+    public List<Product?> ReadAll(Func<Product, bool>? filter = null)
+    {
+        List<Product> productsList = LoadProductFromXml();
+        if (filter == null)
+        {
+            return productsList;
+        }
+        return productsList.Where(p => filter(p)).ToList();
+    }
+
+    public void Update(Product item)
+    {
+        List<Product> productsList = LoadProductFromXml();
+        if (item == null)
+        {
+            throw new DalExceptionNullReceived("Product");
+        }
+        Delete(item.ID);
+        productsList.Add(item);
+        StoreProductToXml(productsList);
+    }
+
+
+    private List<Product> LoadProductFromXml()
+    {
+        try
+        {
+            List<Product> productsList;
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Product>));
+            //קריאת הנתונים מהxml
+            lock (lockObject)
+            {
+                using (FileStream fs = new FileStream("../xml/products.xml", FileMode.Open, FileAccess.Read))
+                {
+                    productsList = serializer.Deserialize(fs) as List<Product>;
+                }
+            }
+            if (productsList == null)
+            {
+                productsList = new List<Product>();
+            }
+            return productsList;
+        }
+        catch
+        {
+            throw new DalExceptionErrorInReadFromXml("Product");
+        }
+    }
+
+    private void StoreProductToXml(List<Product> productsList)
+    {
+        try
+        {
+            lock (lockObject)
+            {
+                using (FileStream fs = new FileStream("../xml/products.xml", FileMode.Open, FileAccess.Write))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(List<Product>));
+                    serializer.Serialize(fs, productsList);
+                }
+            }
+        }
+        catch
+        {
+            throw new DalExceptionErrorInWriteToXml("Product");
+        }
+    }
+}
